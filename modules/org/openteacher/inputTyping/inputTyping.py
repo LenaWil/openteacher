46e142fc777590b0217cacbd78f4328b9998e679
Cas Widdershoven <caswiddershoven@gmail.com>
2011-07-10 16:30:40 +0200

    Added shuffle mode, inputTyping module

---
+++ modules/org/openteacher/inputTyping/inputTyping.py
@@ -0,0 +1,72 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#	Copyright 2011, Cas Widdershoven
+#	Copyright 2011, Marten de Vries
+#
+#	This file is part of OpenTeacher.
+#
+#	OpenTeacher is free software: you can redistribute it and/or modify
+#	it under the terms of the GNU General Public License as published by
+#	the Free Software Foundation, either version 3 of the License, or
+#	(at your option) any later version.
+#
+#	OpenTeacher is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	GNU General Public License for more details.
+#
+#	You should have received a copy of the GNU General Public License
+#	along with OpenTeacher.  If not, see <http://www.gnu.org/licenses/>.
+
+from PyQt4 import QtGui, QtCore
+
+class Result(str):
+	pass
+
+class InputTyping(QtGui.QWidget):
+	def __init__(self, moduleManager, *args, **kwargs):
+		super(InputTyping, self).__init__(*args, **kwargs)
+		self._mm = moduleManager
+
+		self.type = "typingInput"
+
+		self._mm = moduleManager
+		
+		self.checkAnswer = self._mm.createEvent()
+		self.inputLineEdit = QtGui.QLineEdit()
+
+		self.checkButton = QtGui.QPushButton(u"Check!")
+		self.checkButton.setShortcut(QtCore.Qt.Key_Return) #FIXME: translatable?
+		self.correctButton = QtGui.QPushButton(u"Correct anyway")
+
+		mainLayout = QtGui.QGridLayout()
+		mainLayout.addWidget(self.inputLineEdit, 0, 0)
+		mainLayout.addWidget(self.checkButton, 0, 1)
+		mainLayout.addWidget(self.correctButton, 1, 1)
+		self.setLayout(mainLayout)
+
+	def enable(self):
+		self.active = True
+
+	def disable(self):
+		self.active = False
+		
+class InputTypingModule(object):
+	def __init__(self, moduleManager, *args, **kwargs):
+		super(InputTypingModule, self).__init__(*args, **kwargs)
+		self._mm = moduleManager
+
+		self.type = "typingInput"
+
+	def enable(self):
+		self.active = True
+
+	def disable(self):
+		self.active = False
+
+	def createWidget(self):
+		return InputTyping(self._mm)
+
+def init(moduleManager):
+	return InputTypingModule(moduleManager)
---
+++ modules/org/openteacher/teachTypes/shuffleAnswer/shuffleAnswer.py
@@ -0,0 +1,121 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#	Copyright 2011, Cas Widdershoven
+#	Copyright 2011, Marten de Vries
+#
+#	This file is part of OpenTeacher.
+#
+#	OpenTeacher is free software: you can redistribute it and/or modify
+#	it under the terms of the GNU General Public License as published by
+#	the Free Software Foundation, either version 3 of the License, or
+#	(at your option) any later version.
+#
+#	OpenTeacher is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	GNU General Public License for more details.
+#
+#	You should have received a copy of the GNU General Public License
+#	along with OpenTeacher.  If not, see <http://www.gnu.org/licenses/>.
+
+from PyQt4 import QtGui, QtCore
+import random
+
+class Result(str):
+	pass
+
+class TypingTeachWidget(QtGui.QWidget):
+	def __init__(self, moduleManager, *args, **kwargs):
+		super(TypingTeachWidget, self).__init__(*args, **kwargs)
+
+		self._mm = moduleManager
+		self._modules = set(self._mm.mods("active", type="modules")).pop()
+		
+		self.inputWidget = set(self._mm.mods("active", type="typingInput")).pop().createWidget()
+		
+		vbox = QtGui.QVBoxLayout()
+		self.hintLabel = QtGui.QLabel()
+		vbox.addWidget(self.hintLabel)
+		vbox.addWidget(self.inputWidget)
+		self.setLayout(vbox)
+		
+		self.inputWidget.checkAnswer.handle(self.checkAnswer)
+		
+	def setHint(self):
+		hint = QtCore.QCoreApplication.translate("OpenTeacher", "Hint:") + u" "
+		answer = self.word.answers.pop()[0]
+		print answer
+		if len(answer) != 1:
+			while True:
+				hintList = list(answer)
+				random.shuffle(hintList)
+				if hintList != list(answer):
+					hint += u"".join(hintList)
+					break
+				else:
+					continue
+		else:
+			hint += u"."
+		self.hintLabel.setText(hint)
+
+	def updateLessonType(self, lessonType):
+		self.lessonType = lessonType
+
+		self.lessonType.newItem.handle(self.newWord)
+
+		self.inputWidget.checkButton.clicked.connect(self.checkAnswer)
+		self.inputWidget.correctButton.clicked.connect(self.correctLastAnswer)
+
+	def newWord(self, word):
+		try:
+			self.previousWord = self.word
+		except AttributeError:
+			pass
+		self.word = word
+		self.setHint()
+		self.inputWidget.inputLineEdit.clear()
+		self.inputWidget.inputLineEdit.setFocus()
+
+	def correctLastAnswer(self):
+		result = Result("right")
+		result.wordId = self.previousWord.id
+		result.givenAnswer = _(u"Correct anyway") #FIXME: own translation
+		self.lessonType.correctLastAnswer(result)
+		
+	def checkAnswer(self):
+		givenStringAnswer = unicode(self.inputWidget.inputLineEdit.text())
+
+		checkers = set(self._mm.mods("active", type="wordsStringChecker"))
+		try:
+			check = self._modules.chooseItem(checkers).check
+		except IndexError, e:
+			#FIXME: show nice error? Make typing unusable?
+			raise e
+		result = check(givenStringAnswer, self.word)
+
+		self.lessonType.setResult(result)
+
+class TypingTeachTypeModule(object):
+	def __init__(self, moduleManager, *args, **kwargs):
+		super(TypingTeachTypeModule, self).__init__(*args, **kwargs)
+		self._mm = moduleManager
+
+		self.type = "teachType"
+
+	def enable(self):
+		self.dataType = "words"
+		self.name = "Shuffle Answer"
+		self.active = True
+
+	def disable(self):
+		self.active = False
+		del self.dataType
+		del self.name
+
+	def createWidget(self):
+		return TypingTeachWidget(self._mm)
+
+def init(moduleManager):
+	return TypingTeachTypeModule(moduleManager)
+
