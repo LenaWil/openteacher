8ab6d3c2162298f6401db7737da025e21807464a
Milan Boers <milan.boers@gmail.com>
2011-08-21 22:13:55 +0200

    Added interval training. Needs work though.

--- modules/org/openteacher/lessonTypes/interval/interval.py
+++ modules/org/openteacher/lessonTypes/interval/interval.py
@@ -20,51 +20,80 @@
 #	You should have received a copy of the GNU General Public License
 #	along with OpenTeacher.  If not, see <http://www.gnu.org/licenses/>.
 
+import random
+
 """
 Events:
 newItem
 lessonDone
 """
-class AllOnceLessonType(object):
+class IntervalLessonType(object):
 	def __init__(self, moduleManager, list, indexes, *args, **kwargs):
-		super(AllOnceLessonType, self).__init__(*args, **kwargs)
+		super(IntervalLessonType, self).__init__(*args, **kwargs)
 		self._mm = moduleManager
 
 		self.newItem = self._mm.createEvent()
 		self.lessonDone = self._mm.createEvent()
 		self._list = list
 		self._indexes = indexes
-		self._test = {
-			"results": [],
-			"finished": False,
-			"pauses": [],
-		}
+		self._test = []
 
 		self.totalItems = len(self._indexes)
 		self.askedItems = 0
+		
+		# Items in the group
+		self._group = []
 
 	def start(self):
+		# Add items to the group
+		for i in xrange(4):
+			try:
+				self._list["items"][i]
+			except:
+				pass
+			else:
+				self._group.append(i)
 		self._emitNext()
-
+	
+	# result is a Result-type object saying whether the question was answered right or wrong
 	def setResult(self, result):
-		self._test["results"].append(result)
+		self._test.append(result)
 
 		self.askedItems += 1
 		self._emitNext()
 
-	def addPause(self, pause):
-		self._test["pauses"].append(pause)
-
 	def correctLastAnswer(self, result):
-		self._test["results"][-1] = result
+		self._test[-1] = result
 
 	def _emitNext(self):
-		try:
-			i = self._indexes[self.askedItems]
-		except IndexError:
-			#lesson end
-			if len(self._test["results"]) != 0:
-				self._test["finished"] = True
+		# Go through all the items in the group to see which can be removed
+		for i in self._group:
+			right = 0
+			wrong = 0
+			# Fill in right and wrong variables
+			for item in self._test:
+				if item["itemId"] == i:
+					if item["result"] == "right":
+						right += 1
+					elif item["result"] == "wrong":
+						wrong += 1
+			if right + wrong > 2 and \
+			   right / (right + wrong) > 0.8:
+				print "You know " + str(i)
+				# Add new one
+				try:
+					self._list["items"][self._group[-1] + 1]
+				except:
+					pass
+				else:
+					print "adding " + str(self._group[-1] + 1)
+					self._group.append(self._group[-1] + 1)
+				# Remove this item from the group
+				self._group.remove(i)
+		
+		if len(self._group) == 0:
+			# index not in list, so end of lesson
+			if len(self._test) != 0:
 				try:
 					self._list["tests"]
 				except KeyError:
@@ -72,11 +101,17 @@ class AllOnceLessonType(object):
 				self._list["tests"].append(self._test)
 			self.lessonDone.emit()
 		else:
-			self.newItem.emit(self._list["items"][i])
+			if len(self._group) > 1:
+				# Get random number
+				r = random.randrange(0, len(self._group) - 1)
+			else:
+				r = 0
+			self.newItem.emit(self._list["items"][self._group[r]])
+			
 
-class AllOnceModule(object):
+class IntervalModule(object):
 	def __init__(self, moduleManager, *args, **kwargs):
-		super(AllOnceModule, self).__init__(*args, **kwargs)
+		super(IntervalModule, self).__init__(*args, **kwargs)
 		self._mm = moduleManager
 
 		self.type = "lessonType"
@@ -89,7 +124,7 @@ class AllOnceModule(object):
 		)
 
 		self.newItem = self._mm.createEvent()
-		self.name = _("All once")
+		self.name = _("Interval")
 		self.active = True
 
 	def disable(self):
@@ -98,9 +133,9 @@ class AllOnceModule(object):
 		del self.name
 
 	def createLessonType(self, list, indexes):
-		lessonType = AllOnceLessonType(self._mm, list, indexes)
+		lessonType = IntervalLessonType(self._mm, list, indexes)
 		lessonType.newItem.handle(self.newItem.emit)
 		return lessonType
 
 def init(moduleManager):
-	return AllOnceModule(moduleManager)
+	return IntervalModule(moduleManager)
