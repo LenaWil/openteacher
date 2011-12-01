944f045b857b47cb9a0982bd940090233e39a9c0
Marten de Vries <marten94@gmail.com>
2011-12-01 21:27:09 +0100

    Updated module directory structure a bit, comments are now shown in the (words) lesson. Implementes blueprint 'questioncomments'.

---
+++ modules/org/openteacher/enterers/media/mediaEnterer.py
@@ -0,0 +1,343 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#	Copyright 2011, Milan Boers
+#	Copyright 2011, Marten de Vries
+#
+#	This file is part of OpenTeacher.
+#
+#	OpenTeacher is free software: you can redistribute it and/or modify
+#	it under the terms of the GNU General Public License as published by
+#	the Free Software Foundation, either version 3 of the License, or
+#	(at your option) any later version.
+#
+#	OpenTeacher is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	GNU General Public License for more details.
+#
+#	You should have received a copy of the GNU General Public License
+#	along with OpenTeacher.  If not, see <http://www.gnu.org/licenses/>.
+
+from PyQt4 import QtCore
+from PyQt4 import QtGui
+
+import os
+
+
+"""
+The model for the list widget with media items (this construction because without model Qt produces a bug)
+"""
+class EnterItemListModel(QtCore.QAbstractListModel):
+	def __init__(self,items,*args,**kwargs):
+		super(EnterItemListModel, self).__init__(*args, **kwargs)
+		
+		self.listData = []
+		
+		for item in items["items"]:
+			self.listData.append(item["name"])
+	
+	def update(self,items):
+		self.beginInsertRows(QtCore.QModelIndex(), self.rowCount(), self.rowCount())
+		
+		self.listData = []
+		
+		for item in items["items"]:
+			self.listData.append(item["name"])
+		
+		self.endInsertRows()
+	
+	def rowCount(self,parent=QtCore.QModelIndex()):
+		return len(self.listData)
+
+	def data(self,index,role):
+		if index.isValid() and role == QtCore.Qt.DisplayRole:
+			return QtCore.QVariant(self.listData[index.row()])
+		else:
+			return QtCore.QVariant()
+	
+	def textAtIndex(self,index):
+		return self.listData[index]
+
+"""
+The list widget with media items
+"""
+class EnterItemList(QtGui.QListView):
+	def __init__(self,enterWidget,*args,**kwargs):
+		super(EnterItemList, self).__init__(*args, **kwargs)
+		
+		self.enterWidget = enterWidget
+		
+		self.lm = EnterItemListModel(enterWidget.itemList,self)
+		self.setModel(self.lm)
+		self.setSelectionMode(QtGui.QAbstractItemView.SingleSelection)
+	
+	def update(self):
+		self.lm.update(self.enterWidget.itemList)
+	
+	def selectionChanged(self,current,previous):
+		self.setRightActiveItem()
+	
+	def setRightActiveItem(self):
+		if len(self.enterWidget.itemList["items"]) > 0:
+			self.enterWidget.setActiveItem(self.enterWidget.itemList["items"][self.currentIndex().row()])
+
+"""
+The enter tab
+"""
+class EnterWidget(QtGui.QSplitter):
+	def __init__(self,*args, **kwargs):
+		super(EnterWidget, self).__init__(*args, **kwargs)
+		self.itemList = {
+			"items": list(),
+			"tests": list()
+		}
+		
+		self.enterItemList = EnterItemList(self)
+		
+		removeButton = QtGui.QPushButton(_("Remove"))
+		removeButton.clicked.connect(self.removeItem)
+		addLocalButton = QtGui.QPushButton(_("Add local media"))
+		addLocalButton.clicked.connect(self.addLocalItems)
+		addRemoteButton = QtGui.QPushButton(_("Add remote media"))
+		addRemoteButton.clicked.connect(self.addRemoteItems)
+		
+		leftBottom = QtGui.QHBoxLayout()
+		leftBottom.addWidget(removeButton)
+		leftBottom.addWidget(addLocalButton)
+		leftBottom.addWidget(addRemoteButton)
+		
+		left = QtGui.QVBoxLayout()
+		left.addWidget(self.enterItemList)
+		left.addLayout(leftBottom)
+		leftW = QtGui.QWidget()
+		leftW.setLayout(left)
+		
+		self.mediaDisplay = base._modules.default("active", type="mediaDisplay").createDisplay(False)
+		
+		namel = QtGui.QLabel(_("Name:"))
+		
+		self.enterName = QtGui.QLineEdit()
+		self.enterName.textChanged.connect(self.changeName)
+		self.enterName.setEnabled(False)
+		
+		questionl = QtGui.QLabel(_("Question:"))
+		
+		self.enterQuestion = QtGui.QLineEdit()
+		self.enterQuestion.textChanged.connect(self.changeQuestion)
+		self.enterQuestion.setEnabled(False)
+		
+		answerl = QtGui.QLabel(_("Answer:"))
+		
+		self.enterAnswer = QtGui.QLineEdit()
+		self.enterAnswer.textChanged.connect(self.changeAnswer)
+		self.enterAnswer.setEnabled(False)
+		
+		desceditL = QtGui.QVBoxLayout()
+		desceditL.addWidget(namel)
+		desceditL.addWidget(self.enterName)
+		desceditL.addWidget(questionl)
+		desceditL.addWidget(self.enterQuestion)
+		desceditL.addWidget(answerl)
+		desceditL.addWidget(self.enterAnswer)
+		
+		desceditW = QtGui.QWidget()
+		desceditW.setLayout(desceditL)
+		
+		rightSplitter = QtGui.QSplitter(2)
+		rightSplitter.addWidget(self.mediaDisplay)
+		rightSplitter.addWidget(desceditW)
+		
+		right = QtGui.QVBoxLayout()
+		right.addWidget(rightSplitter)
+		rightW = QtGui.QWidget()
+		rightW.setLayout(right)
+		
+		splitter = QtGui.QSplitter(self)
+		splitter.addWidget(leftW)
+		splitter.addWidget(rightW)
+		
+		layout = QtGui.QHBoxLayout()
+		layout.addWidget(splitter)
+		
+		self.setLayout(layout)
+	
+	"""
+	Add items from the local disk to the list
+	"""
+	def addLocalItems(self):
+		extensions = []
+		for module in base._modules.sort("active", type="mediaType"):
+			try:
+				extensions.extend(module.extensions)
+			except AttributeError:
+				# No extensions
+				pass
+		
+		extensionsStr = "("
+		for extension in extensions:
+			extensionsStr += "*" + extension + " "
+		extensionsStr += ")"
+		
+		filenames = QtGui.QFileDialog.getOpenFileNames(self,_("Select file(s)"),QtCore.QDir.homePath(),_("Media") + " " + extensionsStr)
+		for filename in filenames:
+			self.addItem(str(filename), False)
+	
+	"""
+	Add items from the internet to the list
+	"""
+	def addRemoteItems(self):
+		sitenames = []
+		for module in base._modules.sort("active", type="mediaType"):
+			try:
+				sitenames.extend(module.remoteNames)
+			except AttributeError:
+				# No name
+				pass
+		
+		sitenamesStr = ""
+		for sitename in sitenames:
+			sitenamesStr += sitename + ", "
+		sitenamesStr = sitenamesStr[:-2]
+		
+		url, dialog = QtGui.QInputDialog.getText(self, _("File URL"), _("Enter the URL of your website or media item.\nSupported video sites: " + sitenamesStr + "."))
+		if dialog:
+			self.addItem(str(url), True)
+	
+	"""
+	Updates all the widgets if the list has changed
+	"""
+	def updateWidgets(self):
+		self.enterItemList.update()
+	
+	"""
+	Add an item to the list
+	"""
+	def addItem(self,filename,remote=False,name=None,question=None,answer=None):
+		# Check if file is supported
+		for module in base._modules.sort("active", type="mediaType"):
+			if module.supports(filename):
+				item = {
+					"id": int(),
+					"remote": remote,
+					"filename": unicode(filename),
+					"name": unicode(name),
+					"question": unicode(),
+					"answer": unicode()
+				}
+				# Set id
+				try:
+					item["id"] = self.itemList["items"][-1]["id"] +1
+				except IndexError:
+					item["id"] = 0
+				
+				if name != None:
+					item["name"] = name
+				else:
+					if remote == False:
+						item["name"] = os.path.basename(filename)
+					else:
+						item["name"] = filename
+				if question != None:
+					item["question"] = question
+				if answer != None:
+					item["answer"] = answer
+				
+				self.itemList["items"].append(item)
+				self.updateWidgets()
+				break
+		else:
+			QtGui.QMessageBox.critical(self, _("Unsupported file type"), _("This type of file is not supported:\n" + filename))
+	
+	"""
+	Remove an item from the list
+	"""
+	def removeItem(self):
+		self.itemList["items"].remove(self.activeitem)
+		self.updateWidgets()
+		self.mediaDisplay.clear()
+		self.enterName.setText("")
+		self.enterName.setEnabled(False)
+		self.enterQuestion.setText("")
+		self.enterQuestion.setEnabled(False)
+		self.enterAnswer.setText("")
+		self.enterAnswer.setEnabled(False)
+		self.enterItemList.setRightActiveItem()
+	
+	"""
+	Change the active item
+	"""
+	def setActiveItem(self,item):
+		self.activeitem = item
+		self.enterName.setEnabled(True)
+		self.enterName.setText(item["name"])
+		self.enterQuestion.setEnabled(True)
+		self.enterQuestion.setText(item["question"])
+		self.enterAnswer.setEnabled(True)
+		self.enterAnswer.setText(item["answer"])
+		self.mediaDisplay.showMedia(item["filename"], item["remote"], False)
+	
+	"""
+	Change the name of the active item
+	"""
+	def changeName(self):
+		self.activeitem["name"] = unicode(self.enterName.text())
+		self.updateWidgets()
+	
+	
+	"""
+	Change the question of the active item
+	"""
+	def changeQuestion(self):
+		self.activeitem["question"] = unicode(self.enterQuestion.text())
+	
+	"""
+	Change the description of the active item
+	"""
+	def changeAnswer(self):
+		self.activeitem["answer"] = unicode(self.enterAnswer.text())
+	
+
+class MediaEntererModule(object):
+	def __init__(self, moduleManager, *args, **kwargs):
+		super(MediaEntererModule, self).__init__(*args, **kwargs)
+		
+		global base
+		base = self
+		
+		self._mm = moduleManager
+		
+		self.type = "mediaEnterer"
+		
+		self.uses = (
+			self._mm.mods(type="translator"),
+		)
+		self.requires = (
+			self._mm.mods(type="mediaDisplay"),
+		)
+	
+	def enable(self):
+		self._modules = set(self._mm.mods("active", type="modules")).pop()
+		self.active = True
+		
+		#setup translation
+		global _
+		global ngettext
+		
+		try:
+			translator = self._modules.default("active", type="translator")
+		except IndexError:
+			_, ngettext = unicode, lambda a, b, n: a if n == 1 else b
+		else:
+			_, ngettext = translator.gettextFunctions(
+				self._mm.resourcePath("translations")
+			)
+	
+	def disable(self):
+		self.active = False
+	
+	def createMediaEnterer(self):
+		return EnterWidget()
+
+def init(moduleManager):
+	return MediaEntererModule(moduleManager)
---
+++ modules/org/openteacher/teachers/topo/topoTeacher.py
@@ -0,0 +1,377 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#	Copyright 2011, Milan Boers
+#	Copyright 2011, Marten de Vries
+#
+#	This file is part of OpenTeacher.
+#
+#	OpenTeacher is free software: you can redistribute it and/or modify
+#	it under the terms of the GNU General Public License as published by
+#	the Free Software Foundation, either version 3 of the License, or
+#	(at your option) any later version.
+#
+#	OpenTeacher is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	GNU General Public License for more details.
+#
+#	You should have received a copy of the GNU General Public License
+#	along with OpenTeacher.  If not, see <http://www.gnu.org/licenses/>.
+
+from PyQt4 import QtGui
+from PyQt4 import QtCore
+
+import datetime
+
+class Order:
+	Normal, Inversed = xrange(2)
+
+"""
+The dropdown menu to choose lesson type
+"""
+class TeachLessonTypeChooser(QtGui.QComboBox):
+	def __init__(self, teachWidget, *args, **kwargs):
+		super(TeachLessonTypeChooser, self).__init__(*args, **kwargs)
+		
+		self.teachWidget = teachWidget
+		
+		self.currentIndexChanged.connect(self.changeLessonType)
+		
+		self._lessonTypeModules = list(
+			base._mm.mods("active", type="lessonType")
+		)
+		
+		for lessontype in self._lessonTypeModules:
+			self.addItem(lessontype.name, lessontype)
+	
+	"""
+	What happens when you change the lesson type
+	"""
+	def changeLessonType(self, index):
+		if self.teachWidget.inLesson:
+			self.teachWidget.restartLesson()
+	
+	"""
+	Get the current lesson type
+	"""
+	@property
+	def currentLessonType(self):
+		return self._lessonTypeModules[self.currentIndex()]
+
+"""
+The dropdown menu to choose lesson order
+"""
+class TeachLessonOrderChooser(QtGui.QComboBox):
+	def __init__(self, teachWidget, *args, **kwargs):
+		super(TeachLessonOrderChooser, self).__init__(*args, **kwargs)
+		
+		self.currentIndexChanged.connect(self.changeLessonOrder)
+		self.teachWidget = teachWidget
+		
+		self.addItem(_("Place - Name"), 0)
+		self.addItem(_("Name - Place"), 1)
+	
+	"""
+	What happens when you change the lesson order
+	"""
+	def changeLessonOrder(self, index):
+		if self.teachWidget.inLesson:
+			self.teachWidget.restartLesson()
+
+"""
+The lesson itself
+"""
+class TeachTopoLesson(object):
+	def __init__(self, itemList, mapPath, teachWidget, *args, **kwargs):
+		super(TeachTopoLesson, self).__init__(*args, **kwargs)
+		
+		self.teachWidget = teachWidget
+		
+		# Set the map
+		self.teachWidget.mapBox.setMap(mapPath)
+		self.teachWidget.mapBox.setInteractive(self.order)
+		
+		self.itemList = itemList
+		self.lessonType = self.teachWidget.lessonTypeChooser.currentLessonType.createLessonType(self.itemList,range(len(itemList["items"])))
+		
+		self.lessonType.newItem.handle(self.nextQuestion)
+		self.lessonType.lessonDone.handle(self.teachWidget.stopLesson)
+		
+		self.lessonType.start()
+		
+		self.teachWidget.inLesson = True
+		
+		#self.startThinkingTime
+		#self.endThinkingTime
+		
+		# Reset the progress bar
+		self.teachWidget.progress.setValue(0)
+		
+		self.teachWidget.setWidgets(self.order)
+	
+	"""
+	Check whether the given answer was right or wrong
+	"""
+	def checkAnswer(self, answer=None):
+		# Set endThinkingTime if it hasn't been set yet (this is in Name - Place mode)
+		try:
+			self.endThinkingTime
+		except AttributeError:
+			self.endThinkingTime = datetime.datetime.now()
+		
+		active = {
+			"start": self.startThinkingTime,
+			"end": self.endThinkingTime
+		}
+		
+		if self.order == Order.Inversed:
+			if self.currentItem == answer:
+				# Answer was right
+				self.lessonType.setResult({
+					"itemId": self.currentItem["id"],
+					"result": "right",
+					"active": active
+				})
+				# Progress bar
+				self._updateProgressBar()
+			else:
+				# Answer was wrong
+				self.lessonType.setResult({
+					"itemId": self.currentItem["id"],
+					"result": "wrong",
+					"active": active
+				})
+		else:
+			if self.currentItem["name"] == self.teachWidget.answerfield.text():
+				# Answer was right
+				self.lessonType.setResult({
+					"itemId": self.currentItem["id"],
+					"result": "right",
+					"active": active
+				})
+				# Progress bar
+				self._updateProgressBar()
+			else:
+				# Answer was wrong
+				self.lessonType.setResult({
+					"itemId": self.currentItem["id"],
+					"result": "wrong",
+					"active": active
+				})
+		
+		self.teachWidget.listChanged.emit(self.itemList)
+			
+	"""
+	What happens when the next question should be asked
+	"""
+	def nextQuestion(self, item):
+		#set the next question
+		self.currentItem = item
+		if self.order == Order.Inversed:
+			#set the question
+			self.teachWidget.questionLabel.setText(_("Please click this place: ") + self.currentItem["name"])
+		else:
+			#set the arrow to the right position
+			self.teachWidget.mapBox.setArrow(self.currentItem["x"],self.currentItem["y"])
+		# Set the start of the thinking time to now
+		self.startThinkingTime = datetime.datetime.now()
+		# Delete the end of the thinking time
+		try:
+			del self.endThinkingTime
+		except AttributeError:
+			pass
+	
+	"""
+	Ends the lesson
+	"""
+	def endLesson(self):
+		self.teachWidget.inLesson = False
+		
+		# Update and go to results widget, only if the test is progressing
+		try:
+			self.itemList["tests"][-1]
+		except IndexError:
+			pass
+		else:
+			# Go to results widget
+			module = base._modules.default("active", type="resultsDialog")
+			module.showResults(self.itemList, "topo", self.itemList["tests"][-1])
+		
+		self.teachWidget.lessonDone.emit()
+	
+	"""
+	Updates the progress bar
+	"""
+	def _updateProgressBar(self):
+		self.teachWidget.progress.setMaximum(self.lessonType.totalItems+1)
+		self.teachWidget.progress.setValue(self.lessonType.askedItems)
+	
+	@property
+	def order(self):
+		return self.teachWidget.lessonOrderChooser.currentIndex()
+
+"""
+The teach tab
+"""
+class TeachWidget(QtGui.QWidget):
+	lessonDone = QtCore.pyqtSignal()
+	listChanged = QtCore.pyqtSignal([object])
+	def __init__(self, *args, **kwargs):
+		super(TeachWidget, self).__init__(*args, **kwargs)
+		
+		self.inLesson = False
+		
+		## GUI Drawing
+		# Top
+		top = QtGui.QHBoxLayout()
+		
+		label = QtGui.QLabel(_("Lesson type:"))
+		self.lessonTypeChooser = TeachLessonTypeChooser(self)
+		
+		top.addWidget(label)
+		top.addWidget(self.lessonTypeChooser)
+		
+		label = QtGui.QLabel(_("Lesson order:"))
+		self.lessonOrderChooser = TeachLessonOrderChooser(self)
+		
+		top.addWidget(label)
+		top.addWidget(self.lessonOrderChooser)
+		
+		# Middle
+		self.mapBox = base._modules.default("active", type="topoMaps").getTeachMap(self)
+		
+		# Bottom
+		bottom = QtGui.QHBoxLayout()
+		
+		self.label = QtGui.QLabel(_("Which place is here?"))
+		self.answerfield = QtGui.QLineEdit()
+		self.checkanswerbutton = QtGui.QPushButton(_("Check"))
+		self.answerfield.returnPressed.connect(self._checkAnswerButtonClick)
+		self.answerfield.textEdited.connect(self._answerChanged)
+		
+		self.checkanswerbutton.clicked.connect(self._checkAnswerButtonClick)
+		
+		self.questionLabel = QtGui.QLabel(_("Please click this place:"))
+		
+		self.progress = QtGui.QProgressBar()
+		
+		bottom.addWidget(self.label)
+		bottom.addWidget(self.answerfield)
+		bottom.addWidget(self.checkanswerbutton)
+		bottom.addWidget(self.questionLabel)
+		bottom.addWidget(self.progress)
+		
+		# Total
+		layout = QtGui.QVBoxLayout()
+		layout.addLayout(top)
+		layout.addWidget(self.mapBox)
+		layout.addLayout(bottom)
+		
+		self.setLayout(layout)
+	
+	"""
+	Starts the lesson
+	"""
+	def initiateLesson(self, places, mapPath):
+		self.places = places
+		self.mapPath = mapPath
+		
+		self.lesson = TeachTopoLesson(places, mapPath, self)
+		self.answerfield.setFocus()
+	
+	"""
+	Restarts the lesson
+	"""
+	def restartLesson(self):
+		self.initiateLesson(self.places, self.mapPath)
+	
+	"""
+	Stops the lesson
+	"""
+	def stopLesson(self):
+		self.lesson.endLesson()
+		
+		del self.lesson
+	
+	"""
+	What happens when the answer in the textbox has changed
+	"""
+	def _answerChanged(self):
+		try:
+			self.lesson.endThinkingTime
+		except AttributeError:
+			self.lesson.endThinkingTime = datetime.datetime.now()
+		else:
+			if self.answerfield.text() == "":
+				del self.lesson.endThinkingTime
+	
+	"""
+	What happens when you click the check answer button
+	"""
+	def _checkAnswerButtonClick(self):
+		# Check the answer
+		self.lesson.checkAnswer()
+		# Clear the answer field
+		self.answerfield.clear()
+		# Focus the answer field
+		self.answerfield.setFocus()
+	
+	"""
+	Sets the bottom widgets to either the in-order version (False) or the inversed-order (True)
+	"""
+	def setWidgets(self, order):
+		if order == Order.Inversed:
+			self.label.setVisible(False)
+			self.answerfield.setVisible(False)
+			self.checkanswerbutton.setVisible(False)
+			self.questionLabel.setVisible(True)
+		else:
+			self.label.setVisible(True)
+			self.answerfield.setVisible(True)
+			self.checkanswerbutton.setVisible(True)
+			self.questionLabel.setVisible(False)
+
+class TopoTeacherModule(object):
+	def __init__(self, moduleManager, *args, **kwargs):
+		super(TopoTeacherModule, self).__init__(*args, **kwargs)
+		
+		global base
+		base = self
+		
+		self._mm = moduleManager
+		
+		self.type = "topoTeacher"
+		
+		self.uses = (
+			self._mm.mods(type="translator"),
+		)
+		self.requires = (
+			self._mm.mods(type="topoMaps"),
+		)
+	
+	def enable(self):
+		self._modules = set(self._mm.mods("active", type="modules")).pop()
+		self.active = True
+		
+		#setup translation
+		global _
+		global ngettext
+		
+		try:
+			translator = self._modules.default("active", type="translator")
+		except IndexError:
+			_, ngettext = unicode, lambda a, b, n: a if n == 1 else b
+		else:
+			_, ngettext = translator.gettextFunctions(
+				self._mm.resourcePath("translations")
+			)
+	
+	def disable(self):
+		self.active = False
+	
+	def createTopoTeacher(self):
+		return TeachWidget()
+
+def init(moduleManager):
+	return TopoTeacherModule(moduleManager)
