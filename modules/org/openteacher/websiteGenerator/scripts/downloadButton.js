f91c9941d01f736a8a3c98b27e270144179830c3
Milan Boers <milan.boers@gmail.com>
2013-03-11 01:03:53 +0100

    Fixed two bugs in the websiteGenerator. (disabled escaping and fixed downloadbutton)

--- modules/org/openteacher/websiteGenerator/scripts/downloadButton.js
+++ modules/org/openteacher/websiteGenerator/scripts/downloadButton.js
@@ -1,16 +1,16 @@
 $(document).ready(function() {
 	// Set download button
 	$('#osStr').html('for ' + osStr);
-	$('#downloadButton').css('background-image', 'url(../images/oslogos/' + os + '-button.png');
+	$('#downloadButton').css('background-image', 'url(../images/downloadbuttons/' + os + '-button.png');
 	// On mouseover
 	$(document).on('mouseover mouseleave', "#downloadButton", function(event){
 		if (event.type == 'mouseover') {
-			var buttonImage = 'url(../images/' + os + '-button-h.png)';
+			var buttonImage = 'url(../images/downloadbuttons/' + os + '-button-h.png)';
 			$(this).css('background-image', buttonImage);
 		}
 		else
 		{
-			var buttonImage = 'url(../images/' + os + '-button.png)';
+			var buttonImage = 'url(../images/downloadbuttons/' + os + '-button.png)';
 			$(this).css('background-image', buttonImage);
 		}
 	});
